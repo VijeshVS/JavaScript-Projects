1c3fb0c78af2f576638dc24c9f9ed6e74d4d4e8e
Vijesh Shetty <vijeshsshetty@gmail.com>
2023-10-27 07:32:39 +0530

    Calculator done

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,17 +1,24 @@
-buttons = document.querySelectorAll('.number');
-input_obj = document.querySelector('#display');
-console.log(input_obj)
+buttons = document.querySelectorAll('button')
+input_tag = document.querySelector('#display')
 
+let arr = Array.from(buttons)
 
-buttons.forEach((b)=>{
-    b.addEventListener('click',()=>{
-        let k = b.textContent;
-        input_obj.value += k.trim();
+arr.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            let ans = eval(input_tag.value)
+            console.log(ans)
+            input_tag.value = ans
+        }
+        else if(e.target.innerHTML == 'AC'){
+            input_tag.value = ''
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            input_tag.value = input_tag.value.slice(0,input_tag.value.length-1)
+        }
+        else{
+            input_tag.value += e.target.innerHTML
+        }
     })
 })
 
-clearance = document.querySelector('#AC')
-
-clearance.addEventListener('click',()=>{
-    input_obj.value = ''
-})
