a6340ec55ef080140cb31f21aebfe0d09dd1e8a0
Vijesh Shetty <vijeshsshetty@gmail.com>
2023-10-27 13:49:48 +0530

    Countdown Timer done

--- Countdown_Timer/script.js
+++ Countdown_Timer/script.js
@@ -62,10 +62,73 @@ secdown.addEventListener('click',()=>{
 clear.addEventListener('click',()=>{
     min.innerHTML = 0;
     sec.innerHTML = 0;
+
+    minup.disabled = false;
+    mindown.disabled = false;
+    secup.disabled = false;
+    secdown.disabled = false;
+
+    start.innerHTML = 'Start'
+
 })
 
-
+let intervalID = 0;
 
 start.addEventListener('click',()=>{
-    
+    minup.disabled = true;
+    mindown.disabled = true;
+    secup.disabled = true;
+    secdown.disabled = true;
+    start.disabled = true;
+    start.innerHTML = 'Continue';
+
+    if(!intervalID){
+    intervalID = setInterval(()=>{
+        if((Number.parseInt(min.innerHTML) == 0) && (Number.parseInt(sec.innerHTML)) == 0 ){
+            //alert
+            alert('Timeup!!')
+            clearInterval(intervalID);
+            intervalID = 0;
+            start.disabled = false;
+            start.innerHTML = 'Start';
+            
+            // make buttons clickable
+            minup.disabled = false;
+            mindown.disabled = false;
+            secup.disabled = false;
+            secdown.disabled = false;
+
+            console.log('cleared but executed')
+            //clear Interval       
+        }
+        else if ( (Number.parseInt(min.innerHTML) > 0) && (Number.parseInt(sec.innerHTML) == 0) ){
+            let k = Number.parseInt(min.innerHTML);
+            k -= 1;
+            min.innerHTML = k;
+
+            sec.innerHTML = 59;
+        }
+        else if ( (Number.parseInt(min.innerHTML) > 0) && (Number.parseInt(sec.innerHTML) > 0) ){
+            let k = Number.parseInt(sec.innerHTML);
+            k -= 1;
+            sec.innerHTML = k;
+        } else if(Number.parseInt(sec.innerHTML) > 0){
+            let k = Number.parseInt(sec.innerHTML);
+            k -= 1;
+            sec.innerHTML = k;
+        }
+    },1000)
+
+
+}
+    else{
+        alert('Timer has already started!!')
+    }
+   
+})
+
+stop.addEventListener('click',()=>{
+    start.disabled = false;
+    clearInterval(intervalID)
+    intervalID = 0;
 })
